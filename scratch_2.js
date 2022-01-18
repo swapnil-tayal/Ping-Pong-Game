679b33d6af2e608ad4729291818a01d2fafa1710
swapnil <91427197+swapnil-tayal@users.noreply.github.com>
2022-01-18 16:57:53 +0530

    Update scratch_2.js

--- scratch_2.js
+++ scratch_2.js
@@ -1,3 +1,4 @@
+// Variables
 var paddleHeight = 150;
 var paddleWidth = 30;
 var ballRadius = 25;
@@ -13,6 +14,7 @@ var leftSpeedOfBall = 0;
 var score1 = 0;
 var score2 = 0;
 
+//Start Ball Function
 function startBall() {
 	topPositionOfBall = 210;
 	leftPositionOfBall = 520;
@@ -25,6 +27,7 @@ function startBall() {
 	leftSpeedOfBall = side * (Math.random() * 6 + 5);
 };
 
+//Adding Keys Function(Increasing Speed On Key-down)
 document.addEventListener('keydown', function (e) {
      if (e.keyCode == 87 || e.which == 87) { // W key
       speedOfPaddle1 = -10;
@@ -40,7 +43,7 @@ document.addEventListener('keydown', function (e) {
      }
 }, false);
 
-
+//Adding Keys Function(Making Speed Zero On Key-Up)
 document.addEventListener('keyup', function (e) {
 	if (e.keyCode == 87 || e.which == 87) {
 		speedOfPaddle1 = 0;
@@ -56,16 +59,21 @@ document.addEventListener('keyup', function (e) {
 	}
 }, false);
 
-
+//Making An Interval Function To Make Visible Changes 60 times a Second
 window.setInterval(function show() {
 
+    //Adjusting Position of Paddles
 	positionOfPaddle1 += speedOfPaddle1;
 	positionOfPaddle2 += speedOfPaddle2;
 
+    //Adjusting Position of Balls
 	topPositionOfBall += topSpeedOfBall;
 	leftPositionOfBall += leftSpeedOfBall;
 
+    //Make it Single Player (Uncomment Below Statement)
+    //positionOfPaddle2 = topPositionOfBall - paddleHeight/2
 
+    //Conditions for the Paddles so they don't leave the Window
 	if (positionOfPaddle1 <= 1) {
 		positionOfPaddle1 = 1;
 	}
@@ -78,43 +86,46 @@ window.setInterval(function show() {
 	if (positionOfPaddle2 > window.innerHeight - paddleHeight) {
 		positionOfPaddle2 = window.innerHeight - paddleHeight;
 	}
+
+    //Condition For Reflecting back the Ball
 	if (topPositionOfBall <= 10 || topPositionOfBall >= window.innerHeight - ballRadius) {
 		topSpeedOfBall = -topSpeedOfBall
 	}
 
+    //Ball Bounces Back From Paddles
 	if (leftPositionOfBall <= paddleWidth){
 	    if (topPositionOfBall > positionOfPaddle1 && topPositionOfBall < positionOfPaddle1 + paddleHeight){
 	        leftSpeedOfBall = -leftSpeedOfBall;
 	        var audio = new Audio('mixkit-retro-game-notification-212.wav')
-		audio.play()
+            audio.play()
         }else {
             score2++;
             var audio = new Audio('mixkit-falling-empty-can-389.wav')
-	    audio.play()
+            audio.play()
             startBall()}
 	}
 	if (leftPositionOfBall >= window.innerWidth - ballRadius - paddleWidth){
         if (topPositionOfBall > positionOfPaddle2 && topPositionOfBall < positionOfPaddle2 + paddleHeight){
             leftSpeedOfBall = -leftSpeedOfBall;
             var audio = new Audio('mixkit-retro-game-notification-212.wav')
-	    audio.play()
+            audio.play()
         }else {
             score1++
             var audio = new Audio('mixkit-falling-empty-can-389.wav')
-	    audio.play()
+            audio.play()
             startBall()}
     }
 
-    //Make it Single Player
-    //positionOfPaddle2 = topPositionOfBall
-
+    //Updating Position of Paddles
 	document.getElementById("paddle1").style.top = (positionOfPaddle1) + "px";
 	document.getElementById("paddle2").style.top = (positionOfPaddle2) + "px";
 
+    //Updating Position of Ball
 	document.getElementById("ball").style.top = (topPositionOfBall) + "px";
 	document.getElementById("ball").style.left = (leftPositionOfBall) + "px";
 
+    //Updating Scores
     document.getElementById('score1').innerHTML = score1.toString()
     document.getElementById('score2').innerHTML = score2.toString()
 
-}, 1000/60);
+}, 1000/80);
